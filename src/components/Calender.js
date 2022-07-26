e28b83be1299495ecf3f19340aaef1772e9e7ed7
Sajan Tamang <sajantmg2@gmail.com>
2022-07-26 14:21:30 -0400

    calendar widget fixed

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -6,11 +6,9 @@ import {CalendarComponent} from '@syncfusion/ej2-react-calendars';
 
 
 const Calender = () => {
- const dateValue = new Date(new Date().getFullYear(), new Date().getMonth(), 10);
-  const minDate = new Date(new Date().getFullYear(), new Date().getMonth(), 6);
-  const maxDate = new Date(new Date().getFullYear(), new Date().getMonth(), 25);
+ 
   return (
-    <CalendarComponent value={dateValue} min={minDate} max={maxDate}
+    <CalendarComponent 
     isMultiSelection={true}></CalendarComponent>
   );
 };
